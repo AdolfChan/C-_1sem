44a7eb9fdb619cff58bca0e01369733a523635f7
AdolfChan <112843604+AdolfChan@users.noreply.github.com>
2022-12-29 03:25:21 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
+
 #include <iostream>
 #include "BigIntHeader.h"
 using namespace std;
 
-
-
 int main() {
-    BigInt a("1003123123122321312312312312312");
-    BigInt b("100321312312412421313123122");
-    cout << a + 2 << endl;
-    cout << a - b << endl;
-    cout << a * b << endl;
-    cout << b / 4 << endl;
-    cout << a % 12 << endl;
-    return 0;
-    
+	string str = "4213421421421441341312321312312312";
+	BigInt a(str);
+	string str2 = "41344214123123123123123";
+	BigInt b(str2);
+
+	cout << a + 2 << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << b / 4 << endl;
+	cout << a % 12 << endl;
+	return 0;
 }
