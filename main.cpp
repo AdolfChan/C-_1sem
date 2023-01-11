695f25454b74f6b81db7522393ef8617d93b7101
AdolfChan <112843604+AdolfChan@users.noreply.github.com>
2023-01-11 16:18:52 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,13 +6,14 @@ using namespace std;
 int main() {
 	string str = "4213421421421441341312321312312312";
 	BigInt a(str);
-	string str2 = "41344214123123123123123";
-	BigInt b(str2);
-
+	string str2 = "100312323213122";
+	const BigInt b(str2);
 	cout << a + 2 << endl;
-	cout << a - b << endl;
+	cout << ++a << endl;
+	cout << -a-- << endl;
+	cout << b - a << endl;
 	cout << a * b << endl;
-	cout << b / 4 << endl;
-	cout << a % 12 << endl;
+	cout << b / 312 << endl;
+	cout << b % 9 << endl;
 	return 0;
 }
